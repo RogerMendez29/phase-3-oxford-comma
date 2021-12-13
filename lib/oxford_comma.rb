4cddf2535650296782404dd5e2ac46d8c81ce281
Roger Mendez <roger61087@gmail.com>
2021-12-13 12:48:57 -0500

    commit tree 4d27b0c847438fbc1d1fa50c4bb4ad3e0b451bd0

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+    if array.length == 1 
+        array.join
+        
+    
+elsif array.length == 2 
+    array.join(' and ')
+
+else 
+
+    array[array.length - 1] = "and " + array[array.length - 1]
+    array.join (", ")
+    
+    
+    # binding.pry
+    
 end
+    
+end
+
+ puts oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
